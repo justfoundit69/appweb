b488d46c5800e5da92320f5f85027fadb805c43c
justfoundit69 <301148251+justfoundit69@users.noreply.github.com>
2026-07-08 17:35:09 +0700

    Add global header spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             <Header />
             <div className="flex relative">
               <Sidebar />
-              <main className="flex-1 lg:ml-56 min-h-screen">
+              <main className="flex-1 lg:ml-56 min-h-screen pt-24">
                 {children}
               </main>
             </div>
